a81b947a9043c2d25398fe47d2f11ed2cfe66c73
vagrant <vagrant@homestead>
2020-07-07 06:37:08 +0000

    単語問題10問・結果表示処理

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//以下モデルの読み込み（use）
+use App\Question;
+use App\AnswerHistory;
+use App\WrongAnswer;
+use Illuminate\Http\Request;
+
+class WordController extends Controller
+{
+    /**
+     * 問題を出題する処理
+     */
+    function question(Request $request) {
+        $page = $request->session()->get('page', 1);
+
+        $question = Question::where('type', '=', 3)->inRandomOrder()->first();
+        return view('word/question', compact('question', 'page'));
+    }
+
+    /**
+     * 答えをデータベースに保存する処理
+     */
+
+    function answer(Request $request) {
+        AnswerHistory::create([
+            'user_id' => \Auth::id(),
+            'question_id' => $request->question_id,
+            'answer' => $request->answer,
+        ]);
+        $question = Question::where('id', '=', $request->question_id)->first();
+        if ($question->answer !== $request->answer) {
+            //以下wronganswerテーブルに保存する処理
+            WrongAnswer::create([
+                'user_id' => \Auth::id(),
+                'question_id' => $request->question_id,
+                ]);
+        }
+
+        //現在のページを取得
+        $current_page = $request->page;
+        if ($current_page == 10) {
+            return redirect('/word/result');
+        }
+
+        //次のページを取得
+        $next_page = $current_page + 1;
+
+        //次のリクエストまで有効なセッションを作成（ページ数を覚えさせる）
+        $request->session()->flash('page', $next_page);
+
+        return redirect('/word/question');
+    }
+
+    /**
+     * 結果を表示する処理
+     */
+    function result() {
+        return view('word/result');
+    }
+}
--- resources/views/word/question.blade.php
+++ resources/views/word/question.blade.php
@@ -4,13 +4,15 @@
 
     <div class="output">単語問題</div>
 
-    <div class="container">
-    {!!$question->content!!}
+    <div class="m-question">
+      <div class="b-question">{{$page}}.</div>
+      {!!$question->content!!}
     </div>
         <div class="select-answer">
-          <form action="/wordquestion/answer" method="post">
+          <form action="/word/answer" method="post">
           @csrf
             <input type="hidden" name="question_id" value="{{$question->id}}">
+          <input type="hidden" name="page" value="{{$page}}">
               <div class="selection"><button type="submit" name="answer" value="1">1. {{$question->question_1}}</button></div>
               <div class="selection"><button type="submit" name="answer" value="2">2. {{$question->question_2}}</button></div>
               <div class="selection"><button type="submit" name="answer" value="3">3. {{$question->question_3}}</button></div>
