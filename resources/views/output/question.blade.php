84f28e57a5949f24c1cc33eb8399d5f4093c21bf
vagrant <vagrant@homestead>
2020-07-09 15:54:24 +0000

    各questionファイルの修正

--- resources/views/output/question.blade.php
+++ resources/views/output/question.blade.php
@@ -12,11 +12,21 @@
             <form action="/output/answer" method="post">
             @csrf
                 <input type="hidden" name="question_id" value="{{$question->id}}">
-            <input type="hidden" name="page" value="{{$page}}">
-                <div class="selection"><button type="submit" name="answer" value="1">1. {{$question->question_1}}</button></div>
-                <div class="selection"><button type="submit" name="answer" value="2">2. {{$question->question_2}}</button></div>
-                <div class="selection"><button type="submit" name="answer" value="3">3. {{$question->question_3}}</button</div>
-                <div class="selection"><button type="submit" name="answer" value="4">4. {{$question->question_4}}</button></div>
+                <input type="hidden" name="page" value="{{$page}}">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="selection"><button type="submit" name="answer" value="1">1. {{$question->question_1}}</button></div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="selection"><button type="submit" name="answer" value="2">2. {{$question->question_2}}</button></div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="selection"><button type="submit" name="answer" value="3">3. {{$question->question_3}}</button></div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="selection"><button type="submit" name="answer" value="4">4. {{$question->question_4}}</button></div>
+                    </div>
+                </div>
             </form>
         </div>
 @endsection
